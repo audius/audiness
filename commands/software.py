384c0d25bce6583274abdb5d17f19bf8128551de
Fabian Affolter <mail@fabian-affolter.ch>
2024-02-17 16:55:54 +0100

    Allow to update a Nessus instance

---
+++ commands/software.py
@@ -0,0 +1,12 @@
+"""Interact with scans in Nessus."""
+import typer
+
+app = typer.Typer()
+
+
+@app.command()
+def update(ctx: typer.Context):
+    """Update Nessus."""
+    connection = ctx.obj.get("connection")
+
+    connection.software_update.update()    
