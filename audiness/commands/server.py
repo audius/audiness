c24c5455703a66c3bbff90b0e0d566afd2458a9c
Fabian Affolter <mail@fabian-affolter.ch>
2024-03-05 22:05:12 +0100

    Add support for getting the alerts

--- audiness/commands/server.py
+++ audiness/commands/server.py
@@ -1,5 +1,9 @@
 """Interact with the Nessus instance."""
+
 import typer
+from rich.console import Console
+from rich.progress import track
+from rich.table import Table
 
 app = typer.Typer()
 
@@ -12,3 +16,29 @@ def status(ctx: typer.Context):
     status = connection.server.status()
 
     print(status)
+
+
+@app.command()
+def alerts(ctx: typer.Context):
+    """Get the alerts of the Nessus instance."""
+    connection = ctx.obj.get("connection")
+
+    alerts = connection.settings.alerts()
+
+    table = Table(title="Alerts")
+
+    table.add_column("Alert", justify="left", style="cyan", no_wrap=True)
+    table.add_column("Description")
+    table.add_column("Type")
+    table.add_column("Severity")
+
+    for alert in alerts:
+        table.add_row(
+            alert["alert"],
+            alert["description"],
+            alert["type"],
+            str(alert["severity"]),
+        )
+
+    console = Console()
+    console.print(table)
