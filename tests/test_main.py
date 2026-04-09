21b23e06c659e616d8f71d90502660fdaa09b6f8
Fabian Affolter <mail@fabian-affolter.ch>
2026-04-09 10:23:07 +0200

    Add docstrings

--- tests/test_main.py
+++ tests/test_main.py
@@ -8,15 +8,18 @@ runner = CliRunner()
 
 
 def test_app1():
+    """Test if the app raises an error when the access key is not provided."""
     result = runner.invoke(app, ["--access_key"], input="123456\n")
     assert result.exit_code == 2
 
 
 def test_app2():
+    """Test if the app raises an error when the secret key is not provided."""
     result = runner.invoke(app, ["--secret_key"], input=None)
     assert result.exit_code == 2
 
 
 def test_app3():
+    """Test if the app raises an error when the host URL is not provided."""
     result = runner.invoke(app, ["--host"], input="https://localhost:8834\n")
     assert result.exit_code == 2
